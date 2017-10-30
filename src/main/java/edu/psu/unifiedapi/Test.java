af7b885bf97fc5700494c6ae231484cc6b62bfcd
Matthew Benkart <mthwate@mthwate.com>
2017-10-30 14:20:40 -0400

    Added a test APIGateway event handler

---
+++ src/main/java/edu/psu/unifiedapi/Test.java
@@ -0,0 +1,17 @@
+package edu.psu.unifiedapi;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+
+/**
+ * @author mthwate
+ */
+public class Test implements RequestHandler<APIGatewayProxyRequestEvent, String> {
+
+	@Override
+	public String handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+		return "This is a test!";
+	}
+
+}
