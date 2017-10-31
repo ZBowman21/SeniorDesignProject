6be42c4e89851894a6c6c5126408b387df239cc7
brandonalanskas <brandonalanskas@gmail.com>
2017-10-31 00:18:54 -0400

    In Progress Alexa Email Integration

--- alexaskill/src/main/java/edu/psu/alexaskill/PennStateSpeechlet.java
+++ alexaskill/src/main/java/edu/psu/alexaskill/PennStateSpeechlet.java
@@ -12,12 +12,22 @@ import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
 import com.amazon.speech.ui.SimpleCard;
 import com.amazon.speech.ui.OutputSpeech;
+import edu.pennstate.api.*;
+import edu.pennstate.api.model.SendEmailRequest;
 
 public class PennStateSpeechlet implements SpeechletV2 {
 
     @Override
     public void onSessionStarted(SpeechletRequestEnvelope<SessionStartedRequest> requestEnvelope) {
         // any initialization logic goes here
+        PennStateUnified client = PennStateUnified.builder().build();
+        SendEmailRequest newRequest = new SendEmailRequest();
+
+        newRequest.setDestination("bra130@psu.edu");
+        newRequest.setBody("If this works, holy fucking shit.");
+        newRequest.setSubject("Oh hot damn");
+        client.sendEmail(newRequest);
+
     }
 
     @Override
@@ -28,6 +38,25 @@ public class PennStateSpeechlet implements SpeechletV2 {
     @Override
     public SpeechletResponse onIntent(SpeechletRequestEnvelope<IntentRequest> requestEnvelope) {
 
+        Intent intent = requestEnvelope.getRequest().getIntent();
+        String intentName = intent.getName();
+
+        if(requestEnvelope.getRequest().getDialogState().equals(IntentRequest.DialogState.IN_PROGRESS))
+        {
+
+        }
+
+
+        switch(intentName)
+        {
+            case "SendMail":
+                //if( IntentRequest.DialogState == "STARTED")
+                //{
+
+                //}
+
+
+        }
         return getWelcomeResponse();
     }
 
