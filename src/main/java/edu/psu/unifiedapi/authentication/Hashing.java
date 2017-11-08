d92fb69299b3fc04fc8faea1425c436f40bb7254
Matthew Benkart <mthwate@mthwate.com>
2017-11-08 18:35:18 -0500

    Added a hashing class

---
+++ src/main/java/edu/psu/unifiedapi/authentication/Hashing.java
@@ -0,0 +1,16 @@
+package edu.psu.unifiedapi.authentication;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author mthwate
+ */
+public class Hashing {
+
+	public static byte[] hash(String data) throws NoSuchAlgorithmException {
+		MessageDigest md5 = MessageDigest.getInstance("SHA-512");
+		return md5.digest(data.getBytes());
+	}
+
+}
