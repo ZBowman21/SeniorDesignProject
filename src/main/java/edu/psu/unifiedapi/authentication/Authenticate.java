9fc3137cf9052ae381bfdb6315807ab77c3ae536
Matthew Benkart <mthwate@mthwate.com>
2017-11-13 22:50:40 -0500

    Fixed authentication failing on invalid users

--- src/main/java/edu/psu/unifiedapi/authentication/Authenticate.java
+++ src/main/java/edu/psu/unifiedapi/authentication/Authenticate.java
@@ -2,7 +2,7 @@ package edu.psu.unifiedapi.authentication;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.sun.mail.iap.ByteArray;
+import org.apache.http.auth.AUTH;
 
 import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
@@ -14,76 +14,74 @@ import java.sql.SQLException;
 import java.util.Arrays;
 
 public class Authenticate implements RequestHandler<AuthArgs, String> {
+
     private Connection connection;
+
+    public Authenticate() {
+        getConnection();
+    }
+
+    private Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                //TODO log this
+            }
+
+            String url = System.getenv("DB_URL");
+            String user = System.getenv("DB_USER");
+            String pass = System.getenv("DB_PASS");
+
+            try {
+                connection = DriverManager.getConnection(url, user, pass);
+            } catch (SQLException e) {
+                //TODO log this
+            }
+        }
+
+        return connection;
+    }
 
     @Override
     public String handleRequest(AuthArgs aA, Context context) {
 
-        //set up connection
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            context.getLogger().log(e.toString());
-        }
-
-        String url = System.getenv("DB_URL");
-        String user = System.getenv("DB_USER");
-        String pass = System.getenv("DB_PASS");
+        String decryptedPass = null;
 
         try {
-            context.getLogger().log("Timeout " + DriverManager.getLoginTimeout());
-            connection = DriverManager.getConnection(url, user, pass);
-        } catch (SQLException e) {
-            context.getLogger().log(e.toString());
-        }
-
-        //lookup database
-        ResultSet resultSet;
-        byte[] pw = null;
-        String dPW = null;
-        byte[] pwHash = null;
-
-        try {
-            PreparedStatement statement = connection.prepareStatement(
-                    "select username, password, hash from plain_credentials where username = ? AND service = ?");
-            statement.setString(1, aA.username);
-            statement.setString(2, aA.service);
-            statement.execute();
-
-            resultSet = statement.getResultSet();
+            ResultSet resultSet = queryDatabase(aA.username, aA.service);
             if (resultSet.next()) {
-                String un = resultSet.getString(1);
-                pw = resultSet.getBytes(2);
-                pwHash = resultSet.getBytes(3);
-                context.getLogger().log(un.toString());
+                String username = resultSet.getString(1);
+                byte[] encryptedPass = resultSet.getBytes(2);
+                byte[] pwHash = resultSet.getBytes(3);
+
+
+                String decryptedPassTmp = Encryption.decrypt(encryptedPass, aA.passphrase);
+                byte[] pwHashCheck = Hashing.hash(decryptedPassTmp);
+
+                if (Arrays.equals(pwHash, pwHashCheck)) {
+                    decryptedPass = decryptedPassTmp;
+                }
+
+            } else {
+                context.getLogger().log("User '" + aA.username + "' not found in the database");
             }
         } catch (SQLException e) {
-            context.getLogger().log(e.toString());
+            context.getLogger().log("Error accessing database: " + e.getMessage());
+        } catch(GeneralSecurityException e) {
+            context.getLogger().log("Error decrypting password: " + e.getMessage());
         }
 
-        //decrypt
-        //kelp me matt
-        try {
-            dPW = Encryption.decrypt(pw, aA.passphrase);
-        }
-        catch(GeneralSecurityException e) {
-            context.getLogger().log(e.toString());
-        }
-
-        //hash
-        //n kelp me cheese
-        try{
-            pw = Hashing.hash(dPW);
-        }
-        catch (NoSuchAlgorithmException e){
-            context.getLogger().log(e.toString());
-        }
-
-        context.getLogger().log(dPW);
-
-        if (Arrays.equals(pw, pwHash))
-            return dPW;
-        else
-            return null;
+        return decryptedPass;
     }
+
+    private ResultSet queryDatabase(String username, String service) throws SQLException {
+        String queryString = "select username, password, hash from plain_credentials where username = ? AND service = ?";
+        PreparedStatement statement = getConnection().prepareStatement(queryString);
+        statement.setString(1, username);
+        statement.setString(2, service);
+        statement.execute();
+        return statement.getResultSet();
+    }
+
 }
