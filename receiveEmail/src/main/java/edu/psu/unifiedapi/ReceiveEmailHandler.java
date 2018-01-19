a53f25ae594694a728cc00e954c2c7b7ee0e542c
Unknown <zbowman21@gmail.com>
2018-01-19 17:40:28 -0500

    Finish Receive Email
    Misc unused lines removed

--- receiveEmail/src/main/java/edu/psu/unifiedapi/ReceiveEmailHandler.java
+++ receiveEmail/src/main/java/edu/psu/unifiedapi/ReceiveEmailHandler.java
@@ -1,13 +1,10 @@
 package edu.psu.unifiedapi;
 
-import com.amazonaws.services.lambda.invoke.LambdaFunction;
 import com.amazonaws.services.lambda.invoke.LambdaInvokerFactory;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import edu.psu.unifiedapi.account.GetLinkedPlainAccountArgs;
 import edu.psu.unifiedapi.account.IGetLinkedPlainAccount;
-import edu.psu.unifiedapi.auth.Credentials;
-
 import javax.mail.Flags;
 import javax.mail.Folder;
 import javax.mail.Message;
@@ -17,18 +14,16 @@ import javax.mail.Part;
 import javax.mail.Session;
 import javax.mail.Store;
 import javax.mail.search.FlagTerm;
-import java.util.ArrayList;
 import java.util.Properties;
 
 /**
  * @author mthwate and Corey!
  */
-public class ReceiveEmailHandler implements RequestHandler<ReceiveEmailRequest, ArrayList> {
+public class ReceiveEmailHandler implements RequestHandler<ReceiveEmailRequest, EmailObject> {
 
 	@Override
-	public ArrayList<EmailObject> handleRequest(ReceiveEmailRequest input, Context context) {
-		//String retStr = "No messages to display at this time.";
-		ArrayList<EmailObject> returnMessages = new ArrayList<>();
+	public EmailObject handleRequest(ReceiveEmailRequest input, Context context) {
+		EmailObject returnEmail = new EmailObject();
 		Properties props = new Properties();
 
 		//Authentication
@@ -63,28 +58,26 @@ public class ReceiveEmailHandler implements RequestHandler<ReceiveEmailRequest, 
 
 				if(messages.length > 0) {
 					//also return # of unread (size of messages)
-					returnMessages.add(new EmailObject(messages[input.start].getFrom()[0].toString(),
+					returnEmail = new EmailObject(messages[input.start].getFrom()[0].toString(),
 							messages[input.start].getReceivedDate().toString(), messages[input.start].getSubject(),
-							getMessage(messages[input.start]), messages.length));
+							getMessage(messages[input.start]), messages.length);
 				}
 				else{
-					returnMessages.add(new EmailObject("","","","",0));
+					returnEmail = new EmailObject("","","","",0);
 				}
-
 			} catch (MessagingException e) {
 				e.printStackTrace();
-				//context.getLogger().log("Problem retrieving emails: " + e.toString());
+				context.getLogger().log("Problem retrieving emails: " + e.toString());
 			}
-			//context.getLogger().log("Emails retrieved.");
+			context.getLogger().log("Emails retrieved.");
 		}
 		else{
-			//context.getLogger().log("Authentication failed.");
+			context.getLogger().log("Authentication failed.");
 		}
-		return returnMessages;
+		return returnEmail;
 	}
-
 	private String getMessage(Part p){
-		String s = "Could not read the message's content.";
+		String s = "";
 		try {
 			String tmp = p.getContentType();
 			if (tmp.contains("text/plain")) {
--- receiveEmail/src/main/java/edu/psu/unifiedapi/TestReceiveEmail.java
+++ receiveEmail/src/main/java/edu/psu/unifiedapi/TestReceiveEmail.java
@@ -1,21 +1,13 @@
 package edu.psu.unifiedapi;
 
-import edu.psu.unifiedapi.ReceiveEmailHandler;
-import edu.psu.unifiedapi.ReceiveEmailRequest;
-import com.amazonaws.services.lambda.runtime.Context;
-
-import java.util.ArrayList;
-
 /**
  * Created by corey on 11/8/2017.
  */
 public class TestReceiveEmail {
     public static void main(String[] args) {
         ReceiveEmailHandler hand = new ReceiveEmailHandler();
-        ArrayList<EmailObject> s = hand.handleRequest(new ReceiveEmailRequest("mkb5413", "cheese", 4), null);
+        EmailObject s = hand.handleRequest(new ReceiveEmailRequest("mkb5413", "cheese", 4), null);
 
-        for(int i = 0; i < s.size(); i++){
-            System.out.println(s.get(i).from + " " + s.get(i).date + " " + s.get(i).subject + " " + s.get(i).body + " " + s.get(i).unread);
-        }
+        System.out.println(s.from + " " + s.date + " " + s.subject + " " + s.body + " " + s.unread);
     }
 }
