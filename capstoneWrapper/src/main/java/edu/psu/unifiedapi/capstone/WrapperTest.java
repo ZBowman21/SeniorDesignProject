2dc703af89c009aab23e9a38314e94e82f577c91
cuz126 <coreyzalewski@gmail.com>
2018-01-29 23:36:53 -0500

    Fixed test class Ignore this

--- capstoneWrapper/src/main/java/edu/psu/unifiedapi/capstone/WrapperTest.java
+++ capstoneWrapper/src/main/java/edu/psu/unifiedapi/capstone/WrapperTest.java
@@ -1,5 +1,7 @@
 package edu.psu.unifiedapi.capstone;
 
+import edu.psu.unifiedapi.capstoneutils.CapstoneWrapperArgs;
+
 public class WrapperTest {
     public static void main(String[] args) {
         CapstoneWrapperArgs cargs = new CapstoneWrapperArgs();
