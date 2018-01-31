0597921bd4ef6adb7cd6997a29e60491bcb85568
Unknown <zbowman21@gmail.com>
2018-01-30 21:55:37 -0500

    Timezone
    subtracted 18000000ms from the time

--- receiveEmail/src/main/java/edu/psu/unifiedapi/ReceiveEmailHandler.java
+++ receiveEmail/src/main/java/edu/psu/unifiedapi/ReceiveEmailHandler.java
@@ -20,6 +20,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Properties;
+import java.util.TimeZone;
 
 /**
  * @author mthwate and Corey!
@@ -66,9 +67,12 @@ public class ReceiveEmailHandler implements RequestHandler<ReceiveEmailRequest, 
 					String from = messages[messages.length - input.start - 1].getFrom()[0].toString();
 
 					Date d = messages[messages.length - input.start - 1].getReceivedDate();
+
 					Calendar c = Calendar.getInstance();
-					c.setTime(d);
-					String date = new SimpleDateFormat("MMMM dd, yyyy, hh:mm a").format(c.getTime());
+					c.setTimeInMillis(d.getTime() - 18000000);
+
+					SimpleDateFormat estDF = new SimpleDateFormat("MMMM dd, yyyy, hh:mm a");
+					String date = estDF.format(c.getTime());
 
 					String subject =messages[messages.length - input.start - 1].getSubject();
 					String message = getMessage(messages[messages.length - input.start - 1]);
