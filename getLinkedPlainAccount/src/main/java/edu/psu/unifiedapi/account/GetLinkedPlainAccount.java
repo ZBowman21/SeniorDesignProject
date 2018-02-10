befe5b9657d2f81cc2b0588b0225027d8de74076
Matthew Benkart <mthwate@mthwate.com>
2018-02-09 19:27:55 -0500

    Added minor error handling

--- getLinkedPlainAccount/src/main/java/edu/psu/unifiedapi/account/GetLinkedPlainAccount.java
+++ getLinkedPlainAccount/src/main/java/edu/psu/unifiedapi/account/GetLinkedPlainAccount.java
@@ -8,27 +8,21 @@ import edu.psu.unifiedapi.database.Database;
 import java.security.GeneralSecurityException;
 import java.sql.SQLException;
 
-public class GetLinkedPlainAccount implements RequestHandler<GetLinkedPlainAccountArgs, Credentials> {
+public class GetLinkedPlainAccount {
 
     public GetLinkedPlainAccount() {
         Database.init();
     }
 
-    @Override
-    public Credentials handleRequest(GetLinkedPlainAccountArgs aA, Context context) {
+    public Credentials handleRequest(GetLinkedPlainAccountArgs aA, Context context) throws GeneralSecurityException, SQLException, AccountNotFoundException {
 
         Credentials creds = null;
 
-        try {
-            context.getLogger().log("Getting plain credentials for user: " + aA.userId + " and service: " + aA.service);
-            creds = Database.getPlainCredentials(aA.userId, aA.passphrase, aA.service);
-            if (creds == null) {
-                context.getLogger().log("User '" + aA.userId + "' not found in the database");
-            }
-        } catch (SQLException e) {
-            context.getLogger().log("Error accessing database: " + e.getMessage());
-        } catch(GeneralSecurityException e) {
-            context.getLogger().log("Error decrypting password: " + e.getMessage());
+        context.getLogger().log("Getting plain credentials for user: " + aA.userId + " and service: " + aA.service);
+        creds = Database.getPlainCredentials(aA.userId, aA.passphrase, aA.service);
+        if (creds == null) {
+            context.getLogger().log("User '" + aA.userId + "' not found in the database");
+            throw new AccountNotFoundException();
         }
 
         return creds;
