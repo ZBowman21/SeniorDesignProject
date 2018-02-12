68aa3398a8fdbf261f8fd45d5370b209bf4ad1bc
Matthew Benkart <mthwate@mthwate.com>
2018-02-11 20:09:49 -0500

    Added repeatable handler annotation

--- pennStateAlexaSkill/src/main/java/edu/psu/alexaskill/intent_handlers/Handler.java
+++ pennStateAlexaSkill/src/main/java/edu/psu/alexaskill/intent_handlers/Handler.java
@@ -1,10 +1,8 @@
 package edu.psu.alexaskill.intent_handlers;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Repeatable(Handlers.class)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Handler
