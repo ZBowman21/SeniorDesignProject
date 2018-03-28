61a50830e415676df7bf7615c4a3287bc7bae625
Matthew Benkart <mthwate@mthwate.com>
2018-03-27 22:46:21 -0400

    Added generics and slf4j logging to CapstoneWrapper

--- capstoneWrapper/src/main/java/edu/psu/unifiedapi/capstone/CapstoneWrapper.java
+++ capstoneWrapper/src/main/java/edu/psu/unifiedapi/capstone/CapstoneWrapper.java
@@ -1,16 +1,16 @@
 package edu.psu.unifiedapi.capstone;
 
 import com.amazonaws.services.lambda.invoke.LambdaInvokerFactory;
-import com.amazonaws.services.lambda.runtime.Context;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import edu.psu.unifiedapi.account.GetLinkedTokenAccountArgs;
 import edu.psu.unifiedapi.account.IGetLinkedTokenAccount;
 import edu.psu.unifiedapi.account.IUpdateLinkedTokenAccount;
 import edu.psu.unifiedapi.account.UpdateLinkedTokenAccountArgs;
-import edu.psu.unifiedapi.auth.Credentials;
 import edu.psu.unifiedapi.capstone.utils.CapstoneResponse;
 import edu.psu.unifiedapi.restclientutil.RestClient;
-import com.google.api.client.json.JsonObjectParser;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.StringReader;
@@ -18,16 +18,18 @@ import java.io.StringReader;
 
 public class CapstoneWrapper{
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     private final String baseUrl = "http://capstone.bd.psu.edu:8090";
     private String username, path, params;
 
-    public CapstoneWrapper(String username, String path, String params){
+    public CapstoneWrapper(String username, String path, String params) {
         this.username = username;
         this. path = path;
         this. params = params == null ? "" : params;
     }
 
-    public CapstoneResponse CapCall(Class responseClass, Context context) throws CapstoneException{
+    public <T extends CapstoneResponse> T capCall(Class<T> responseClass) throws CapstoneException {
 
         GetLinkedTokenAccountArgs ta = new GetLinkedTokenAccountArgs();
         ta.userId = username;
@@ -39,7 +41,7 @@ public class CapstoneWrapper{
         // Regex...
         String[] authId = token.split(" ");
 
-        context.getLogger().log("Using authid = " + authId[1] + " and nounce = " + authId[0]);
+		logger.info("Using authid = {} and nounce = {}", authId[1], authId[0]);
 
         CapstoneAuth capAuth = new CapstoneAuth(authId[1], authId[0]);
 
@@ -48,22 +50,22 @@ public class CapstoneWrapper{
 
         JacksonFactory jack = new JacksonFactory();
         JsonObjectParser parser = new JsonObjectParser(jack);
-        CapstoneResponse capRes = new CapstoneResponse();
+        T capRes = null;
         //capRes.response = ""; // to avoid null pointer exception
         try {
-            capRes = (CapstoneResponse)parser.parseAndClose(new StringReader(response), responseClass);
+            capRes = parser.parseAndClose(new StringReader(response), responseClass);
         } catch (IOException e) {
-            context.getLogger().log(e.getMessage());
+			logger.info(e.getMessage());
         }
 
-        if(!capRes.valid)
+        if(capRes == null || !capRes.valid)
             throw new CapstoneException("Failed to authenticate with Capstone, must request new Authentication Object from Capstone.");
 
         // manipulate the capRes.Auth here....
         UpdateLinkedTokenAccountArgs ua = new UpdateLinkedTokenAccountArgs();
         ua.username = username;
         ua.service = ta.service;
-        context.getLogger().log(response);
+		logger.info(response);
         ua.value = authId[0] + " " + capRes.AuthenticateObject.NounceCode;
 
         IUpdateLinkedTokenAccount updateToken = LambdaInvokerFactory.builder().build(IUpdateLinkedTokenAccount.class);
